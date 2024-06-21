fd82078dcdfc3b4036c114e98cfd01549427612e
sumitumariya <159953111+sumitumariya@users.noreply.github.com>
2024-06-21 10:15:23 +0530

    Update tictactoegame.py

--- tictactoegame.py
+++ tictactoegame.py
@@ -5,9 +5,9 @@ from tkinter import messagebox
 def check_winner():
     for combo in [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]:
         if buttons[combo[0]]["text"] ==buttons[combo[1]]["text"] == buttons[combo[2]]["text"] !="":
-            buttons[combo[0]].config(bg="skyblue")
-            buttons[combo[1]].config(bg="skyblue")
-            buttons[combo[2]].config(bg="skyblue")
+            buttons[combo[0]].config(bg="blue")
+            buttons[combo[1]].config(bg="blue")
+            buttons[combo[2]].config(bg="blue")
             messagebox.showinfo("Tic-Tac-Toe",f"Player {buttons[combo[0]]['text']}wins!")
             root.quit()
 
